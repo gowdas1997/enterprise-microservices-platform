183a4b0bdf3eb9f86fb48a6a927ae0687bf98fda
gowdas1997 <gowdaskr1997@gmail.com>
2026-04-30 13:13:46 +0530

    caheckoutservice6

--- src/checkoutservice/main.go
+++ src/checkoutservice/main.go
@@ -208,6 +208,10 @@ func (cs *checkoutService) Watch(req *healthpb.HealthCheckRequest, ws healthpb.H
 	return status.Errorf(codes.Unimplemented, "health check via Watch not implemented")
 }
 
+func (cs *checkoutService) List(ctx context.Context, req *healthpb.HealthListRequest) (*healthpb.HealthListResponse, error) {
+	return &healthpb.HealthListResponse{}, nil
+}
+
 func (cs *checkoutService) PlaceOrder(ctx context.Context, req *pb.PlaceOrderRequest) (*pb.PlaceOrderResponse, error) {
 	log.Infof("[PlaceOrder] user_id=%q user_currency=%q", req.UserId, req.UserCurrency)
 
